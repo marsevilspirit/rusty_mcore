253354eca065c0c62057779528161505e132540c
marsevilspirit <marsevilspirit@gmail.com>
2024-10-26 02:55:36 +0800

    success: 成功脱离操作系统运行rust code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 #![no_std]
+#![no_main]
 
-fn main() {}
+use core::panic::PanicInfo;
+
+// no_mangle经验篇能够禁用名称重整
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+// 这个函数将在 panic 时被调用
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
