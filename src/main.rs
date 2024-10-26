eb0a363ad51c67393d40de9339679df64ff21e1c
marsevilspirit <marsevilspirit@gmail.com>
2024-10-26 19:25:37 +0800

    add vga_budder

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,20 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 // 这个函数将在 panic 时被调用
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
-
-static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle] // 不重整函数名
 pub extern "C" fn _start() -> ! {
     // 因为编译器会寻找一个名为 `_start` 的函数，所以这个函数就是入口点
     // 默认命名为 `_start`
 
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for(i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
-    loop {}
+    println!("hello World{}", "!");
+    panic!("Some panic message");
 }
